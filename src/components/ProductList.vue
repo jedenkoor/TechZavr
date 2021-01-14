3846a3e9834496966a21a10dfcb15007307d7997
Danil Kuzmin <dazekamikadze19962002@gmail.com>
2021-01-14 14:56:24 +0300

    first commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem v-for="(product, index) in products" :key="index" :product="product"/>
+  </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem'
+export default {
+  props: [
+    'products'
+  ],
+  components: {
+    ProductItem
+  }
+}
+</script>
---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Часы Apple Watch Series 5 GPS + Cellular 44mm Stainless Steel Case with Milanese Loop',
+    price: 75990,
+    image: '/img/products/1.jpg'
+  },
+  {
+    title: 'Смартфон Samsung Galaxy S20 Ultra 12/128G',
+    price: 75950,
+    image: '/img/products/2.jpg'
+  },
+  {
+    title: 'Планшет Apple iPad Pro 12.9 (2018) 64Gb Wi-Fi',
+    price: 75490,
+    image: '/img/products/3.png'
+  },
+  {
+    title: 'Смартфон Apple iPhone 11 Pro 64GB',
+    price: 75390,
+    image: '/img/products/4.png'
+  },
+  {
+    title: 'Неттоп Apple Mac Mini MXNF2RU/A Intel Core i3-8100/8 ГБ/256 ГБ SSD/Intel UHD Graphics 630/OS X',
+    price: 70990,
+    image: '/img/products/5.jpg'
+  },
+  {
+    title: 'Apple iPad Pro 11 (2020) 256GB Wi-Fi',
+    price: 75180,
+    image: '/img/products/6.jpg'
+  },
+  {
+    title: 'Игровая приставка Sony PlayStation 5 Digital Edition',
+    price: 68990,
+    image: '/img/products/7.jpg'
+  },
+  {
+    title: 'Смартфон Apple iPhone 12 64GB',
+    price: 70380,
+    image: '/img/products/8.jpg'
+  },
+  {
+    title: 'Квадрокоптер DJI Mavic Air Fly More Combo',
+    price: 68490,
+    image: '/img/products/9.jpg'
+  },
+  {
+    title: 'Ноутбук Apple MacBook Air 13 Mid 2017 (Intel Core i5 1.8GHz / RAM 8GB / SSD 128GB) MQD32',
+    price: 67280,
+    image: '/img/products/10.jpg'
+  },
+  {
+    title: 'Часы Garmin Fenix 5X Plus Sapphire',
+    price: 65990,
+    image: '/img/products/11.jpg'
+  },
+  {
+    title: 'Смартфон Samsung Galaxy Note 9 512GB',
+    price: 71990,
+    image: '/img/products/12.png'
+  },
+]
